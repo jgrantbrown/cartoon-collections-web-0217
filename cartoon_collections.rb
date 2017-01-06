6d88eaa6f1a44bff307809f672015dda783bf79d
jason brown <j.grant.brown@gmail.com>
2017-01-06 18:42:27 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,34 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves)# code an argument here
   # Your code here
+  puts dwarves.map.with_index { |name, index| "#{index+1} #{name}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(planeteer_calls)# code an argument here
   # Your code here
+  return planeteer_calls.collect.with_index {|name|  "#{name.capitalize}!"}
+
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(planeteer_calls)
+  planeteer_calls.any? {|name| name.length>4}
+=begin
+any?() public
+Passes each element of the collection to the given block.
+The method returns true if the block ever returns a value other than false or nil.
+If the block is not given, Ruby adds an implicit block of {|obj| obj}
+(that is any? will return true if at least one of the collection members is not false or nil.
+http://apidock.com/ruby/Enumerable/any%3F
+=end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-end
+
+  array.any? {|x| if cheese_types.include?(x)
+              return "#{x}"
+              end}
+else
+  return nil
+
+  end
